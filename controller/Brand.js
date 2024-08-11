342b16f06e89ef9dcf7598aabdfc9315f6b987f0
Danish <danish.j@greyorange.com>
2024-08-11 21:09:29 +0530

    commited first

--- controller/Brand.js
+++ controller/Brand.js
@@ -1,12 +1,12 @@
 const { Brand } = require('../model/Brand');
 
 exports.fetchBrands = async (req, res) => {
-  try {
-    const brands = await Brand.find({}).exec();
-    res.status(200).json(brands);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  // try {
+  //   const brands = await Brand.find({}).exec();
+  //   res.status(200).json(brands);
+  // } catch (err) {
+  //   res.status(400).json(err);
+  // }
 };
 
 exports.createBrand = async (req, res) => {
